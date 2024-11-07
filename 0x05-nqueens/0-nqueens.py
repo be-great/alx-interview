27a0a69ca5d230c3fbcecd025c4000c4110a1ba8
be-great <begreat5333@gmail.com>
2024-11-07 20:53:54 +0300

    update task 0

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -4,17 +4,12 @@ import sys
 
 def is_safe(board, row, col, N):
     """is the data is safe"""
-    # Check this row on the left side
     for i in range(col):
         if board[row][i] == 1:
             return False
-
-    # Check the upper diagonal on the left side
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-
-    # Check the lower diagonal on the left side
     for i, j in zip(range(row, N), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
@@ -25,7 +20,6 @@ def is_safe(board, row, col, N):
 def solve_nqueens_util(board, col, N, solutions):
     """solve the nqueens util"""
     if col == N:
-        # Collect solution as list of positions
         solution = []
         for i in range(N):
             for j in range(N):
@@ -38,7 +32,7 @@ def solve_nqueens_util(board, col, N, solutions):
         if is_safe(board, i, col, N):
             board[i][col] = 1
             solve_nqueens_util(board, col + 1, N, solutions)
-            board[i][col] = 0  # Backtrack
+            board[i][col] = 0
 
 
 def solve_nqueens(N):
